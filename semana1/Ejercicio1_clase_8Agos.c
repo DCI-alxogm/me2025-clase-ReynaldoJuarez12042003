b41026a6a5e4fefff65f9463dd600ff1c0649223
ReynaldoJuarez12042003 <reynaldojuarezaranda@gmail.com>
2025-08-08 11:54:47 -0600

    Add files via upload

---
+++ semana1/Ejercicio1_clase_8Agos.c
@@ -0,0 +1,23 @@
+// Ejercicio en clase 8 de agosto, Reynaldo JUarez Aranda
+/* Hacer un programa que use arreglos y que imprima
+5 numeros flotantes y el valor de las funciones*/
+
+ #include <stdio.h>
+ #include <math.h>
+
+ int main () {
+ int i;
+ float arreglo[5];
+ arreglo[0]=4;
+arreglo[1]=3;
+arreglo[2]=1.5;
+arreglo[3]=8.9;
+arreglo[4]=7.1;
+
+printf( "%f %f %f %f %f", arreglo[0], arreglo[1], arreglo[2], arreglo[3], arreglo[4]);
+
+// f(x)= log(x+1)
+
+
+
+ }
