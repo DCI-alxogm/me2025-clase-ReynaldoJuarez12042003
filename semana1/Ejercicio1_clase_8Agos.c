12013b8bf1eeedfaadcedb079ffecde518e60185
ReynaldoJuarez12042003 <reynaldojuarezaranda@gmail.com>
2025-08-08 19:02:37 -0600

    Add files via upload

--- semana1/Ejercicio1_clase_8Agos.c
+++ semana1/Ejercicio1_clase_8Agos.c
@@ -6,18 +6,43 @@
  #include <math.h>
 
  int main () {
- int i;
+
  float arreglo[5];
  arreglo[0]=4;
 arreglo[1]=3;
-arreglo[2]=1.5;
-arreglo[3]=8.9;
-arreglo[4]=7.1;
+arreglo[2]=2;
+arreglo[3]=9;
+arreglo[4]=7;
+float funcion1 [5];
+float funcion2[5];
+float funcion3[5];
 
-printf( "%f %f %f %f %f", arreglo[0], arreglo[1], arreglo[2], arreglo[3], arreglo[4]);
 
-// f(x)= log(x+1)
 
+for(int i=0; i<5; i=i+1){
+  funcion1[i]=arreglo[i]*arreglo[i];
+  funcion2[i]=log((arreglo[i]+1));
+  funcion3[i]=exp(arreglo[i])+cos(arreglo[i]);
+}
+printf("\n");
+printf("F1 x^2:\n");
+for(int i=0; i<5; i=i+1) {
+  printf("%f\t", funcion1[i]);
+}
+//En esta parte el log es natural
+printf("\n");
+printf("F2 log(x+1):\n");
+for(int i=0; i<5; i=i+1) {
+  printf("%f\t", funcion2[i]);
+}
+
+//El calculo del cos(x) lo hace en radianes C.
+printf("\n");
+printf("F3 exp(x)+cos(x):\n");
+for(int i=0; i<5; i=i+1) {
+  printf("%f\t", funcion3[i]);
+}
+return 0;
 
 
  }
