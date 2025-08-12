eedb971a8fcd74ef0f861c637e07151f20b61b18
ReynaldoJuarez12042003 <reynaldojuarezaranda@gmail.com>
2025-08-12 11:52:44 -0600

    Add files via upload

---
+++ Semana2/Agosto12_ej1.c
@@ -0,0 +1,40 @@
+//Reynaldo Juarez Aranda 
+//12 de agosto 2025
+
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  int numeros[5];
+  float resultados [5];
+
+  int indx;
+  float pi=3.1415926536;
+
+
+  numeros[0]=1;
+  numeros[1]=2;
+  numeros[2]=2;
+  numeros[3]=4;
+  numeros[4]=5;
+
+  //printf("x\t x^2 \t log(x+1) \t exp(x)+cos(x)\n");
+  for (indx=0; indx<5; indx++) {
+   // printf("%d %d\n", numeros[indx], numeros[indx]%2);
+   if(numeros[indx]%2==0){
+    resultados[indx]=log10(numeros[indx]+1);
+    printf("x=%d \n log(x+1)= %f\n", numeros[indx], resultados[indx]);
+   }else{
+    resultados[indx]=exp(numeros[indx])+cos(numeros[indx]);
+    printf("x=%d \n exp(x)+cos(x)=%f\n", numeros[indx], resultados[indx]);
+   }
+   // printf("\%f %f %f %f\n", numeros [indx], numeros[indx]*numeros[indx], log10(numeros[indx]+1), exp(numeros[indx])+cos(numeros[indx]));
+  }
+
+  printf("Resultados\n");
+  for(indx=0; indx<5; indx++){
+    printf("%f\n", resultados[indx]);
+  }
+  printf("Pi+Pi=%.16lf", pi+pi);
+}
