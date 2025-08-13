c2e9a71981e8e1f57f53fb29c25754319a442c04
ReynaldoJuarez12042003 <reynaldojuarezaranda@gmail.com>
2025-08-13 16:01:58 -0600

    Add files via upload

---
+++ Semana2/operaciones_int.c
@@ -0,0 +1,16 @@
+//Jerarquia de operaciones, 13 de agosto de 2025
+//Reynaldo Hassan Juarez Aranda
+
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+  int a=3, b=4, c=5, d=7;
+  int e;
+
+  printf("e=(a+b)*c/d=%d\n", (a+b)*c/d);
+  printf("e=((a+b)*c)/d=%d\n", ((a+b)*c)/d);
+  printf("e=a+b*c/d=%d\n", a+b*c/d);
+  printf("e=a+(b*c)/d=%d\n", a+(b*c)/d);
+}
+  
