5a40752ed4cd6fdc846953f40d2ab9a2b4a333f0
ReynaldoJuarez12042003 <reynaldojuarezaranda@gmail.com>
2025-08-15 11:56:19 -0600

    Add files via upload

---
+++ Semana2/Sol_cuadratica_Agos15.c
@@ -0,0 +1,24 @@
+//Reynaldo Hassan Juarez Aranda 
+// 15 Agosto 2025
+
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+  //Definir las variables en tipo float
+  float a,b,c,x1,x2,discri;
+  float xi1=0,xi2=0;
+
+  //Solicitar y guardar los valores de a, b y c
+  printf("Escribe el valor de a:");
+  scanf("%f", &a);
+
+  printf("Escribe el valor de b:");
+  scanf("%f", &b);
+
+  printf("Escribe el valor de c:");
+  scanf("%f", &c);
+
+  
+
+}
