4f16f49e79e94d414bf307841ee480dcd54cfaf6
ReynaldoJuarez12042003 <reynaldojuarezaranda@gmail.com>
2025-09-19 23:17:44 -0600

    Add files via upload

---
+++ Semana 7/Newton-Raphson_Modificado.c
@@ -0,0 +1,41 @@
+/*Reynaldo Hassan Juarez Aranda 19/09/25
+Newton Raphson Modificado*/
+
+#include <stdio.h>
+#include <math.h>
+
+//Declaramos f(x),f´(x),f´´(x)
+double f(double x) {
+  return exp(-x)-x;
+}
+
+double df(double x) {
+  return -exp(-x)-1;
+}
+
+double df2(double x) {
+  return 1/exp(x);
+}
+
+int main() {
+  double E_max=1e-8,x0=0,x1, Error;
+  int contador=0;
+
+  printf("Los resultados para encontrar la raiz de f(x)=(e^-x) -x son:\n");
+
+  do{
+    x1=x0-(f(x0)*df(x0))/((df(x0)*df(x0))-(f(x0)*(df2(x0))));
+    Error=fabs((x1-x0)/x1);
+    contador++;
+
+    printf("Iteracion %d:\n", contador);
+    printf("x%d=%.9lf\n", contador, x1);
+    printf("Error aproximado=%.9lf \n\n", Error);
+
+    x0=x1;
+  }while(Error>=E_max);
+  printf("Se alcanzo el Emax establecido: %.9lf\n", E_max);
+  printf("Ultima aproximacion a la raiz: x=%.9lf\n", x1);
+  return 0;
+
+}
